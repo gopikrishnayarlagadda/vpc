e9c531c7f7971e6df479bbedb6184388e1147190
gopikrishnayarlagadda <yarlagaddagopi84@gmail.com>
2021-07-19 12:45:57 +0530

    first commit

---
+++ ntier-tf-aws/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-west-2"
+}
+# we need to create a vpc resource
+resource "aws_vpc" "ntiervpc" {
+    cidr_block = "192.168.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+    tags = {
+      "Name" = "from tf"
+    }
+  
+}
+# we need to create a subnet web1
+resource "aws_subnet" "web1" {
+  vpc_id = aws_vpc.ntiervpc.id
+  cidr_block = "192.168.0.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+      "Name" = "web1"
+    }
+
+  
+    
+}
