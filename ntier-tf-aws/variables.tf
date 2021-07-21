0fc9a2937b9cb27910ccfc9ba8629d78a7086ff1
gopikrishnayarlagadda <yarlagaddagopi84@gmail.com>
2021-07-21 18:37:32 +0530

    az's

--- ntier-tf-aws/variables.tf
+++ ntier-tf-aws/variables.tf
@@ -24,14 +24,3 @@ variable "subnets" {
     description = "Name of the subnets"
 
 }
-
-variable "subnetazs" {
-
-    type = list(string)
-
-    default = ["us-west-2a","us-west-2b","us-west-2a","us-west-2b","us-west-2a","us-west-2b"]
-
-
-
-
-} 
